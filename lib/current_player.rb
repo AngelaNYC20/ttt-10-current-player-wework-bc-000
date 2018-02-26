15ce22acaf9f60f5e5f0915d92e95b138fdc974b
Angela A <ranran.wang@wework.com>
2018-02-26 22:00:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,15 @@ count
 end 
   
 def current_player(board)
-turn_count
+  def turn_count(board)
+    count = 0 
+    board.each do |position|
+      if position == "X" || position == "O"
+      count += 1
+  end
+end 
+  count 
+end 
+
+
 
