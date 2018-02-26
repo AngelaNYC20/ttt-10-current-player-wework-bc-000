f8a7d423d2579ab0642b6819843a49655d4533e4
Angela A <ranran.wang@wework.com>
2018-02-26 21:56:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,3 @@
-# We will need to keep track of the number of turns by setting some sort of counter and starting it at 0. We will need to iterate over each member of the board array, check to see if that element is an "X" or an "O". If it is, we increment our counter variable by 1.
 
 def turn_count(board)
   count = 0 
